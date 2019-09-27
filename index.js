e72eff1c685fd1ccc72a1ad5f04880fba58012f2
Soumya Rauth <shoummorauth@live.co.uk>
2019-09-27 22:53:04 +0600

    git test commit to check whether it asks for password or not

--- index.js
+++ index.js
@@ -3,12 +3,14 @@
  * Here the variable is declared after it's initialization 
  * But still it will work because during the compilation, JS will pull all off it's declaration 
  * to the top of the script. As a result var1 here will have a declaration available
+ * But let will display error because the concept of hoisting does not apply on let
  */
 
 if(true){
     var1=2;
     console.log(var1)
         var var1;
-
 }
+
+
 
