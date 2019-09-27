e58a1ab02553b4f405aeeccfb636dd5044af702c
Soumya Rauth <shoummorauth@live.co.uk>
2019-09-27 22:48:33 +0600

    hoisting example in javascript

---
+++ index.js
@@ -0,0 +1,14 @@
+/**
+ * Hoisting 
+ * Here the variable is declared after it's initialization 
+ * But still it will work because during the compilation, JS will pull all off it's declaration 
+ * to the top of the script. As a result var1 here will have a declaration available
+ */
+
+if(true){
+    var1=2;
+    console.log(var1)
+        var var1;
+
+}
+
