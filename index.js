089e410c1cfff9965a489354ef2fde4176381b87
Soumya Rauth <shoummorauth@live.co.uk>
2019-09-28 22:16:50 +0600

    two separate functions with var and let

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
-/**
- * Hoisting 
- * Here the variable is declared after it's initialization 
- * But still it will work because during the compilation, JS will pull all off it's declaration 
- * to the top of the script. As a result var1 here will have a declaration available
- * But let will display error because the concept of hoisting does not apply on let and constant
- */
-
-if(true){
-    var1=2;
-    console.log(var1)
-        var var1;
-}
-
-
-
+//Printing two different functions; one with let and the other with var
+(function(){
+    if(true){
+        let count = 2;
+        console.log(`count is ${count}`)
+    }
+})();
+(function(){
+    if(true){
+        var count = 2;
+    }
+    console.log(`count is ${count}`)
+})()
